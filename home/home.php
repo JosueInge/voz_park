f93c8cc011e8af12ead986cb6c945280f78fe5c8
Josue <wilfredojosue438@gmail.com>
2026-04-26 16:17:12 -0600

    actualizacion de infraestructura

---
+++ home/home.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+<meta charset="UTF=8">
+<title>En desarrollo</title>
+<link rel="stylesheet" href="estilos.css">
+
+</head>
+
+<body>
+
+<div class="header">
+    <img src="imagenes/logo.jpeg" alt="Logo" class="logo">
+
+    <div class="nav">
+        <a href="#">INICIAR SESION</a>
+        <a href="#">REGISTRATE</a>
+    </div>
+</div>
+
+<div class="main">
+    <h1>LO SENTIMOS</h1>
+    <h2>ESTE SITIO ESTA EN DESARROLLO &lt;/&gt;</h2>
+
+    <img src="imagenes/imagen.png" alt="En desarrollo">
+
+    <div class="footer">
+        Powered by: IVENQO
+    </div>
+</div>
+
+</body>
+</html>
